f7dad02ba5b4de134e3c0abf8fee3328f2e6d27e
drumonii <drumonii@users.noreply.github.com>
2017-09-14 23:00:26 -0400

    Convert BuildTest to @JsonTest

--- src/test/java/com/drumonii/loltrollbuild/model/BuildTest.java
+++ src/test/java/com/drumonii/loltrollbuild/model/BuildTest.java
@@ -1,30 +1,123 @@
 package com.drumonii.loltrollbuild.model;
 
-import com.drumonii.loltrollbuild.BaseSpringTestRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import org.springframework.boot.test.json.ObjectContent;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Fail.fail;
 
-public class BuildTest extends BaseSpringTestRunner {
+@RunWith(SpringRunner.class)
+@JsonTest
+public class BuildTest {
+
+	@Autowired
+	private JacksonTester<Build> jacksonTester;
+
+	@Test
+	public void serializesIntoJson() {
+		Build build = new Build();
+		build.setId(1);
+		build.setCreatedDate(LocalDateTime.now());
+		build.setChampionId(1);
+		build.setItem1Id(1);
+		build.setItem2Id(2);
+		build.setItem3Id(3);
+		build.setItem4Id(4);
+		build.setItem5Id(5);
+		build.setItem6Id(6);
+		build.setSummonerSpell1Id(1);
+		build.setSummonerSpell2Id(2);
+		build.setTrinketId(7);
+		build.setMapId(1);
+
+		JsonContent<Build> jsonContent = null;
+		try {
+			jsonContent = jacksonTester.write(build);
+		} catch (IOException e) {
+			fail("Unable to serialize Build into JSON", e);
+		}
+
+		assertThat(jsonContent).hasJsonPathNumberValue("$.champion");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.item1");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.item2");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.item3");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.item4");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.item5");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.item6");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.summonerSpell1");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.summonerSpell2");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.trinket");
+		assertThat(jsonContent).hasJsonPathNumberValue("$.map");
+	}
+
+	@Test
+	public void deserializesFromJson() {
+		String json =
+				"{ " +
+				"  \"createdDate\": \"2017-09-14T22:55:26.47\", " +
+				"  \"id\": 1, " +
+				"  \"champion\": 1, " +
+				"  \"item1\": 1, " +
+				"  \"item2\": 2, " +
+				"  \"item3\": 3, " +
+				"  \"item4\": 4, " +
+				"  \"item5\": 5, " +
+				"  \"item6\": 6, " +
+				"  \"summonerSpell1\": 1, " +
+				"  \"summonerSpell2\": 2, " +
+				"  \"trinket\": 7, " +
+				"  \"map\": 1 " +
+				"}";
+
+		ObjectContent<Build> build = null;
+		try {
+			build = jacksonTester.parse(json);
+		} catch (IOException e) {
+			fail("Unable to deserialize Build from JSON", e);
+		}
+
+		assertThat(build.getObject()).isNotNull();
+		assertThat(build.getObject().getCreatedDate()).isNotNull();
+		assertThat(build.getObject().getId()).isOne();
+		assertThat(build.getObject().getChampionId()).isOne();
+		assertThat(build.getObject().getItem1Id()).isOne();
+		assertThat(build.getObject().getItem2Id()).isEqualTo(2);
+		assertThat(build.getObject().getItem3Id()).isEqualTo(3);
+		assertThat(build.getObject().getItem4Id()).isEqualTo(4);
+		assertThat(build.getObject().getItem5Id()).isEqualTo(5);
+		assertThat(build.getObject().getItem6Id()).isEqualTo(6);
+		assertThat(build.getObject().getTrinketId()).isEqualTo(7);
+		assertThat(build.getObject().getSummonerSpell1Id()).isOne();
+		assertThat(build.getObject().getSummonerSpell2Id()).isEqualTo(2);
+		assertThat(build.getObject().getMapId()).isOne();
+	}
 
 	@Test
 	public void equals() {
 		Build build1 = new Build();
-		build1.setChampionId(championsResponse.getChampions().get("Braum").getId());
-		build1.setItem1Id(itemsResponse.getItems().get("3009").getId());
-		build1.setItem2Id(itemsResponse.getItems().get("3151").getId());
-		build1.setItem3Id(itemsResponse.getItems().get("3142").getId());
-		build1.setItem4Id(itemsResponse.getItems().get("3147").getId());
-		build1.setItem5Id(itemsResponse.getItems().get("3157").getId());
-		build1.setItem6Id(itemsResponse.getItems().get("3001").getId());
-		build1.setSummonerSpell1Id(summonerSpellsResponse.getSummonerSpells().get("SummonerHeal").getId());
-		build1.setSummonerSpell2Id(summonerSpellsResponse.getSummonerSpells().get("SummonerHaste").getId());
-		build1.setTrinketId(itemsResponse.getItems().get("3341").getId());
-		build1.setMapId(mapsResponse.getMaps().get(SUMMONERS_RIFT).getMapId());
-		build1 = buildsRepository.save(build1);
+		build1.setChampionId(1);
+		build1.setItem1Id(1);
+		build1.setItem2Id(2);
+		build1.setItem3Id(3);
+		build1.setItem4Id(4);
+		build1.setItem5Id(5);
+		build1.setItem6Id(6);
+		build1.setSummonerSpell1Id(1);
+		build1.setSummonerSpell2Id(2);
+		build1.setTrinketId(7);
+		build1.setMapId(1);
 
 		Build build2 = new Build();
-		build2.setChampion(build1.getChampion());
+		build2.setChampionId(build1.getChampionId());
 		build2.setItem1Id(build1.getItem1Id());
 		build2.setItem2Id(build1.getItem2Id());
 		build2.setItem3Id(build1.getItem3Id());
@@ -35,11 +128,10 @@ public class BuildTest extends BaseSpringTestRunner {
 		build2.setSummonerSpell2Id(build1.getSummonerSpell2Id());
 		build2.setTrinketId(build1.getTrinketId());
 		build2.setMapId(build1.getMapId());
-		build2 = buildsRepository.save(build1);
 
 		assertThat(build1).isEqualTo(build2);
 
-		build2.setItem2Id(itemsResponse.getItems().get("3083").getId());
+		build2.setItem2Id(100);
 		assertThat(build1).isNotEqualTo(build2);
 	}
 
