5067fd8e818d8aa5f107f97637a9eb287ba1a221
drumonii <drumonii@users.noreply.github.com>
2017-12-10 00:58:13 -0500

    Convert AdminLoginControllerTest to @WebMvcTest

--- src/test/java/com/drumonii/loltrollbuild/admin/AdminLoginControllerTest.java
+++ src/test/java/com/drumonii/loltrollbuild/admin/AdminLoginControllerTest.java
@@ -1,9 +1,20 @@
 package com.drumonii.loltrollbuild.admin;
 
-import com.drumonii.loltrollbuild.BaseSpringTestRunner;
 import com.drumonii.loltrollbuild.annotation.WithMockAdminUser;
+import com.drumonii.loltrollbuild.config.WebSecurityConfig;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
+import static com.drumonii.loltrollbuild.config.Profiles.TESTING;
 import static com.drumonii.loltrollbuild.config.WebSecurityConfig.ADMIN_ROLE;
 import static com.drumonii.loltrollbuild.config.WebSecurityConfig.WebDevTestingSecurityConfig.IN_MEM_PASSWORD;
 import static com.drumonii.loltrollbuild.config.WebSecurityConfig.WebDevTestingSecurityConfig.IN_MEM_USERNAME;
@@ -16,18 +27,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-public class AdminLoginControllerTest extends BaseSpringTestRunner {
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = AdminLoginController.class, excludeFilters = @Filter(ControllerAdvice.class))
+@Import(WebSecurityConfig.class)
+@ActiveProfiles({ TESTING })
+public class AdminLoginControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@WithAnonymousUser
 	@Test
-	public void adminCanLoginAndLogout() throws Exception {
+	public void adminCanLogin() throws Exception {
 		mockMvc.perform(formLogin("/admin/login").user(IN_MEM_USERNAME).password(IN_MEM_PASSWORD))
 				.andExpect(authenticated().withRoles(ADMIN_ROLE))
 				.andExpect(redirectedUrl("/admin"));
+	}
+
+	@WithMockAdminUser
+	@Test
+	public void adminCanLogout() throws Exception {
 		mockMvc.perform(logout("/admin/logout"))
 				.andExpect(unauthenticated())
 				.andExpect(redirectedUrl("/admin/login?logout"));
 	}
 
+	@WithAnonymousUser
 	@Test
 	public void getLoginAsNotLoggedIn() throws Exception {
 		mockMvc.perform(get("/admin/login"))
