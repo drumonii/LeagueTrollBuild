77728c2ad40a2b572d95ba3e3669dfc0f214e0ea
drumonii <drumonii@users.noreply.github.com>
2018-01-24 00:11:17 -0500

    Convert RetrievalJobsSchedulingTest to @RunWith(JUnit4.class)

--- src/test/java/com/drumonii/loltrollbuild/batch/scheduling/RetrievalJobsSchedulingTest.java
+++ src/test/java/com/drumonii/loltrollbuild/batch/scheduling/RetrievalJobsSchedulingTest.java
@@ -1,7 +1,8 @@
 package com.drumonii.loltrollbuild.batch.scheduling;
 
-import com.drumonii.loltrollbuild.BaseSpringTestRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.scheduling.support.SimpleTriggerContext;
 
@@ -12,15 +13,16 @@ import java.util.Date;
 import static com.drumonii.loltrollbuild.batch.scheduling.RetrievalJobsScheduling.CRON_SCHEDULE;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RetrievalJobsSchedulingTest extends BaseSpringTestRunner {
+@RunWith(JUnit4.class)
+public class RetrievalJobsSchedulingTest {
 
 	@Test
-	public void allRetrievalsCronExpression() throws Exception {
+	public void allRetrievalsCronExpression() {
 		CronTrigger cronTrigger = new CronTrigger(CRON_SCHEDULE);
 		Date triggerDate = Date.from(LocalDateTime.now().withHour(4).withMinute(0).withSecond(0).withNano(0)
 				.atZone(ZoneId.systemDefault()).toInstant());
-		LocalDateTime nextExecutionTime = LocalDateTime.ofInstant(cronTrigger.nextExecutionTime(new
-				SimpleTriggerContext(triggerDate, triggerDate, triggerDate)).toInstant(), ZoneId.systemDefault());
+		LocalDateTime nextExecutionTime = LocalDateTime.ofInstant(cronTrigger.nextExecutionTime(
+				new SimpleTriggerContext(triggerDate, triggerDate, triggerDate)).toInstant(), ZoneId.systemDefault());
 		assertThat(nextExecutionTime)
 				.isEqualTo(LocalDateTime.now().plusDays(1).withHour(4).withMinute(0).withSecond(0).withNano(0));
 	}
