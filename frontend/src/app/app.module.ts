14a7e70014fb504b0a3485dd0ebae0612562e0d8
drumonii <drumonii@users.noreply.github.com>
2018-03-15 00:00:51 -0400

    HttpInterceptor to prepend the base URL to each request

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+import { BaseUrlHttpInterceptor } from './interceptor/base-url.http-interceptor';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: BaseUrlHttpInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
