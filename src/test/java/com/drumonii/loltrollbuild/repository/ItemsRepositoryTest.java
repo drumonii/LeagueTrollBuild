bcf8da77ab4b654c446ce556641abfe0092f0d0a
drumonii <drumonii@users.noreply.github.com>
2018-03-17 15:58:22 -0400

    Remove Repeat as there's no randomizing

--- src/test/java/com/drumonii/loltrollbuild/repository/ItemsRepositoryTest.java
+++ src/test/java/com/drumonii/loltrollbuild/repository/ItemsRepositoryTest.java
@@ -10,7 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Repeat;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -96,7 +95,6 @@ public abstract class ItemsRepositoryTest {
 				.contains(true);
 	}
 
-	@Repeat(15)
 	@Test
 	public void forTrollBuild() {
 		List<Item> forTrollBuild = itemsRepository.forTrollBuild(SUMMONERS_RIFT_ID);
