bf50c0fd6695baedc3b207c793b6d86c69d98c59
drumonii <drumonii@users.noreply.github.com>
2018-03-17 14:43:35 -0400

    getChampions in ChampionsService

--- frontend/src/app/service/champions.service.spec.ts
+++ frontend/src/app/service/champions.service.spec.ts
@@ -1,15 +1,89 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ChampionsService } from './champions.service';
+
+import { Champion } from '@model/champion';
 
 describe('ChampionsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ChampionsService]
     });
   });
 
-  it('should be created', inject([ChampionsService], (service: ChampionsService) => {
-    expect(service).toBeTruthy();
+  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    httpMock.verify();
+  }));
+
+  it('should get Champions', inject([ChampionsService, HttpTestingController],
+    (service: ChampionsService, httpMock: HttpTestingController) => {
+    const mockChampions: Champion[] = [
+      {
+        id: 67,
+        key: 'Vayne',
+        name: 'Vayne',
+        title: 'the Night Hunter',
+        partype: 'Mana',
+        info: {
+          attack: 10,
+          defense: 1,
+          magic: 1,
+          difficulty: 8
+        },
+        spells: [], // omitted for brevity
+        passive: {
+          name: 'Night Hunter',
+          description: 'Vayne ruthlessly hunts evil-doers, gaining 30 movement speed when moving toward nearby enemy ' +
+            'champions.',
+          image: {
+            full: 'Vayne_NightHunter.png',
+            sprite: 'passive4.png',
+            group: 'passive',
+            imgSrc: [],
+            x: 0,
+            y: 0,
+            w: 48,
+            h: 48
+          }
+        },
+        image: {
+          full: 'Vayne.png',
+          sprite: 'champion4.png',
+          group: 'champion',
+          imgSrc: [],
+          x: 0,
+          y: 0,
+          w: 48,
+          h: 48
+        },
+        tags: [
+          'Assassin',
+          'Marksman'
+        ]
+      }
+    ];
+
+    service.getChampions().subscribe(champions => {
+      expect(champions).toEqual(mockChampions);
+    });
+
+    const testReq = httpMock.expectOne('/api/champions');
+    expect(testReq.request.method).toEqual('GET');
+
+    testReq.flush(mockChampions);
+  }));
+
+  it('should get Champions with REST error', inject([ChampionsService, HttpTestingController],
+    (service: ChampionsService, httpMock: HttpTestingController) => {
+    service.getChampions().subscribe(champions => {
+      expect(champions).toEqual([]);
+    });
+
+    const testReq = httpMock.expectOne('/api/champions');
+    expect(testReq.request.method).toEqual('GET');
+
+    testReq.error(new ErrorEvent('An unexpected error occurred'));
   }));
 });
--- frontend/src/app/service/champions.service.ts
+++ frontend/src/app/service/champions.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError } from 'rxjs/operators';
+
+import { Champion } from '@model/champion';
 
 @Injectable()
 export class ChampionsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getChampions(): Observable<Champion[]> {
+    return this.httpClient.get<Champion[]>('/api/champions')
+      .pipe(
+        catchError((error) => {
+          console.error(`Caught error while GETing Champions: ${JSON.stringify(error)}`);
+          return of([]);
+        })
+      );
+  }
 
 }
