5c355297569e1c842f65966d0cb13c5cbe612f15
drumonii <drumonii@users.noreply.github.com>
2018-03-18 15:38:22 -0400

    Module alias for layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-import { FooterComponent } from './layout/footer/footer.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from '@layout/footer/footer.component';
+import { HeaderComponent } from '@layout/header/header.component';
 
 import { ChampionsService } from '@service/champions.service';
 import { GameMapsService } from '@service/game-maps.service';
