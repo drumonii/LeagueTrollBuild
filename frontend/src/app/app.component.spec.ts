420f44fc40c5ebc8c91223b670653b0e534a3de0
drumonii <drumonii@users.noreply.github.com>
2018-03-21 00:36:00 -0400

    Add header and footer components to app component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { FooterComponent } from '@layout/footer/footer.component';
+import { HeaderComponent } from '@layout/header/header.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        FooterComponent,
+        HeaderComponent
       ],
       imports: [
         AppRoutingModule
