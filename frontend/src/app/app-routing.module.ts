72dc7ea3f9566ede00ab4a93d8a8b7554770a6c3
drumonii <drumonii@users.noreply.github.com>
2018-04-07 20:49:30 -0400

    Add champion page routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ChampionPage } from '@page/champion/champion.page';
 import { ChampionsPage } from '@page/champions/champions.page';
 
 const routes: Routes = [
   { path: 'champions', component: ChampionsPage },
+  { path: 'champions/:name', component: ChampionPage },
   { path: '', redirectTo: '/champions', pathMatch: 'full' },
 ];
 
