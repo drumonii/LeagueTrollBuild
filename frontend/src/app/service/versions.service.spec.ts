f04c4c84e3d749bd6552a6039273135580778659
drumonii <drumonii@users.noreply.github.com>
2018-04-17 21:25:08 -0400

    getLatestVersion in versions service

--- frontend/src/app/service/versions.service.spec.ts
+++ frontend/src/app/service/versions.service.spec.ts
@@ -1,15 +1,45 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { VersionsService } from './versions.service';
+
+import { Version } from '@model/version';
 
 describe('VersionsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [VersionsService]
     });
   });
 
-  it('should be created', inject([VersionsService], (service: VersionsService) => {
-    expect(service).toBeTruthy();
+  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    httpMock.verify();
+  }));
+
+  it('should get the latest saved Version', inject([VersionsService, HttpTestingController],
+    (service: VersionsService, httpMock: HttpTestingController) => {
+    const mockLatestVersion = new Version('8.7.1');
+
+    service.getLatestVersion().subscribe(latestVersion => {
+      expect(latestVersion).toEqual(mockLatestVersion);
+    });
+
+    const testReq = httpMock.expectOne('/api/versions/latest');
+    expect(testReq.request.method).toEqual('GET');
+
+    testReq.flush(mockLatestVersion);
+  }));
+
+  it('should get the latest saved Version with REST error', inject([VersionsService, HttpTestingController],
+    (service: VersionsService, httpMock: HttpTestingController) => {
+    service.getLatestVersion().subscribe(latestVersion => {
+      expect(latestVersion).toBeNull();
+    });
+
+    const testReq = httpMock.expectOne('/api/versions/latest');
+    expect(testReq.request.method).toEqual('GET');
+
+    testReq.error(new ErrorEvent('An unexpected error occurred'));
   }));
 });
--- frontend/src/app/service/versions.service.ts
+++ frontend/src/app/service/versions.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError } from 'rxjs/operators';
+
+import { Version } from '@model/version';
 
 @Injectable()
 export class VersionsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getLatestVersion(): Observable<Version> {
+    return this.httpClient.get<Version>('/api/versions/latest')
+      .pipe(
+        catchError((error) => {
+          console.error(`Caught error while GETing latest Version: ${JSON.stringify(error)}`);
+          return of(null);
+        })
+      );
+  }
 
 }
