cbca5eb86fc41b902515a2fab1131500ffb7781d
drumonii <drumonii@users.noreply.github.com>
2018-06-04 23:12:00 -0400

    Add builds page routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { BuildsPage } from '@page/builds/builds.page';
 import { ChampionPage } from '@page/champion/champion.page';
 import { ChampionsPage } from '@page/champions/champions.page';
 
 const routes: Routes = [
+  { path: 'builds', redirectTo: '/champions', pathMatch: 'full' },
+  { path: 'builds/:buildId', component: BuildsPage },
   { path: 'champions', component: ChampionsPage },
   { path: 'champions/:name', component: ChampionPage },
   { path: '', redirectTo: '/champions', pathMatch: 'full' },
