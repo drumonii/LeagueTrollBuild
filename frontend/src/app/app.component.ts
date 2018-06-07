a2834a6b961f8c760f2bb861cbc88b83039f12d2
drumonii <drumonii@users.noreply.github.com>
2018-06-06 23:52:15 -0400

    Prefix AppComponent selector with ltb

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ltb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
