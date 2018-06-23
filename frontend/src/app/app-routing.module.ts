4a52da43a151e26a05946b5d60eb07841fc4160b
drumonii <drumonii@users.noreply.github.com>
2018-06-22 22:15:03 -0400

    Set ** path to NotFoundPage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BuildsPage } from '@page/builds/builds.page';
 import { ChampionPage } from '@page/champion/champion.page';
 import { ChampionsPage } from '@page/champions/champions.page';
+import { NotFoundPage } from '@page/error/not-found.page';
 
 const routes: Routes = [
   { path: 'builds', redirectTo: '/champions', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'champions', component: ChampionsPage },
   { path: 'champions/:name', component: ChampionPage },
   { path: '', redirectTo: '/champions', pathMatch: 'full' },
+  { path: '**', component: NotFoundPage }
 ];
 
 @NgModule({
