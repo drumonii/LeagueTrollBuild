8047021957ef0cd0bf682e709b57bb0dab35ec86
drumonii <drumonii@users.noreply.github.com>
2018-08-19 17:26:35 -0400

    Test each map of ItemsRepository calls

--- backend/src/test/java/com/drumonii/loltrollbuild/repository/ItemsRepositoryTest.java
+++ backend/src/test/java/com/drumonii/loltrollbuild/repository/ItemsRepositoryTest.java
@@ -4,6 +4,7 @@ import com.drumonii.loltrollbuild.model.Item;
 import com.drumonii.loltrollbuild.model.ItemGold;
 import com.drumonii.loltrollbuild.riot.api.ItemsResponse;
 import com.drumonii.loltrollbuild.test.repository.RepositoryTest;
+import com.drumonii.loltrollbuild.util.GameMapUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.iterable.Extractor;
 import org.junit.Before;
@@ -17,7 +18,9 @@ import java.util.Objects;
 import java.util.SortedMap;
 import java.util.function.Consumer;
 
+import static com.drumonii.loltrollbuild.util.GameMapUtil.HOWLING_ABYSS_ID;
 import static com.drumonii.loltrollbuild.util.GameMapUtil.SUMMONERS_RIFT_ID;
+import static com.drumonii.loltrollbuild.util.GameMapUtil.TWISTED_TREELINE_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -32,6 +35,8 @@ public abstract class ItemsRepositoryTest {
 
 	private ItemsResponse itemsResponse;
 
+	private static final List<Integer> MAPS = List.of(TWISTED_TREELINE_ID, SUMMONERS_RIFT_ID, HOWLING_ABYSS_ID);
+
 	protected abstract ItemsResponse getItemsResponse();
 
 	@Before
@@ -44,37 +49,48 @@ public abstract class ItemsRepositoryTest {
 	public void boots() {
 		Item bootsOfSpeed = itemsResponse.getItems().get("1001");
 
-		List<Item> boots = itemsRepository.boots(SUMMONERS_RIFT_ID);
-		assertThat(boots).isNotEmpty();
-		assertThat(boots).doesNotHaveDuplicates();
-		assertThat(boots).doesNotContain(bootsOfSpeed);
-		assertThat(boots).flatExtracting(Item::getFrom)
-				.contains(1001);
-		assertThat(boots).extracting(Item::getDescription).allSatisfy((Consumer<String>) description -> {
-			assertThat(description).isNotNull();
-			assertThat(description).contains("Movement");
-		});
-		assertThat(boots).extracting(Item::getMaps)
-				.extracting((Extractor<SortedMap<Integer, Boolean>, Object>) input -> input.get(SUMMONERS_RIFT_ID))
-				.containsOnly(true);
+		for (Integer map : MAPS) {
+			List<Item> boots = itemsRepository.boots(map);
+			assertThat(boots).as("Boots should be empty for map: " + GameMapUtil.getNameFromId(map) + "(" + map + ")")
+					.isNotEmpty();
+			assertThat(boots).doesNotHaveDuplicates();
+			assertThat(boots).doesNotContain(bootsOfSpeed);
+			assertThat(boots).flatExtracting(Item::getFrom)
+					.contains(1001);
+			assertThat(boots).extracting(Item::getDescription).allSatisfy((Consumer<String>) description -> {
+				assertThat(description).isNotNull();
+				assertThat(description).contains("Movement");
+			});
+			assertThat(boots).extracting(Item::getMaps)
+					.extracting((Extractor<SortedMap<Integer, Boolean>, Object>) input -> input.get(map))
+					.containsOnly(true);
+		}
 	}
 
 	@Test
 	public void trinkets() {
-		List<Item> trinkets = itemsRepository.trinkets(SUMMONERS_RIFT_ID);
-		assertThat(trinkets).isNotEmpty();
-		assertThat(trinkets).doesNotHaveDuplicates();
-		assertThat(trinkets).extracting(Item::getGold).extracting(ItemGold::getTotal)
-				.containsOnly(0);
-		assertThat(trinkets).extracting(Item::getGold).extracting("purchasable", Boolean.class)
-				.containsOnly(true);
-		assertThat(trinkets).extracting(Item::getMaps)
-				.extracting((Extractor<SortedMap<Integer, Boolean>, Object>) input -> input.get(SUMMONERS_RIFT_ID))
-				.containsOnly(true);
-		assertThat(trinkets).extracting(Item::getDescription).allSatisfy((Consumer<String>) description -> {
-			assertThat(description).isNotNull();
-			assertThat(description).contains("Trinket");
-		});
+		for (Integer map : MAPS) {
+			List<Item> trinkets = itemsRepository.trinkets(map);
+			if (map == HOWLING_ABYSS_ID) {
+				assertThat(trinkets).as("Trinkets should be empty for map: " + GameMapUtil.getNameFromId(map) + "(" + map + ")")
+						.isEmpty();
+			} else {
+				assertThat(trinkets).as("Trinkets should not be empty for map: " + GameMapUtil.getNameFromId(map) + "(" + map + ")")
+						.isNotEmpty();
+				assertThat(trinkets).doesNotHaveDuplicates();
+				assertThat(trinkets).extracting(Item::getGold).extracting(ItemGold::getTotal)
+						.containsOnly(0);
+				assertThat(trinkets).extracting(Item::getGold).extracting("purchasable", Boolean.class)
+						.containsOnly(true);
+				assertThat(trinkets).extracting(Item::getMaps)
+						.extracting((Extractor<SortedMap<Integer, Boolean>, Object>) input -> input.get(map))
+						.containsOnly(true);
+				assertThat(trinkets).extracting(Item::getDescription).allSatisfy((Consumer<String>) description -> {
+					assertThat(description).isNotNull();
+					assertThat(description).contains("Trinket");
+				});
+			}
+		}
 	}
 
 	@Test
@@ -97,48 +113,51 @@ public abstract class ItemsRepositoryTest {
 
 	@Test
 	public void forTrollBuild() {
-		List<Item> forTrollBuild = itemsRepository.forTrollBuild(SUMMONERS_RIFT_ID);
-		assertThat(forTrollBuild).isNotEmpty();
-		assertThat(forTrollBuild).doesNotHaveDuplicates();
-		assertThat(forTrollBuild).extracting(Item::getMaps)
-				.extracting((Extractor<SortedMap<Integer, Boolean>, Object>) input -> input.get(SUMMONERS_RIFT_ID))
-				.contains(true);
-		assertThat(forTrollBuild).extracting(Item::getGold)
-				.extracting("purchasable", Boolean.class)
-				.containsOnly(true);
-		assertThat(forTrollBuild).extracting(Item::getConsumed)
-				.containsNull();
-		assertThat(forTrollBuild).filteredOn(item -> item.getInto() != null).flatExtracting(Item::getInto)
-				.isEmpty();
-		assertThat(forTrollBuild).doesNotContain(itemsResponse.getItems().get("1001"));
-		assertThat(forTrollBuild).extracting(Item::getDescription).allSatisfy((Consumer<String>) description -> {
-			assertThat(description).isNotNull();
-			assertThat(description).doesNotContain("Movement");
-			assertThat(description).doesNotContain("Potion");
-			assertThat(description).doesNotContain("Trinket");
-		});
-		assertThat(forTrollBuild).extracting(Item::getRequiredChampion).allSatisfy((Consumer<String>) requiredChampion -> {
-			assertThat(requiredChampion).isNull();
-		});
-		assertThat(forTrollBuild).extracting(Item::getRequiredAlly).allSatisfy((Consumer<String>) requiredAlly -> {
-			assertThat(requiredAlly).isNull();
-		});
-		assertThat(forTrollBuild).extracting(Item::getName).allSatisfy((Consumer<String>) name -> {
-			assertThat(name).isNotNull();
-			assertThat(name).doesNotContain("Movement");
-			assertThat(name).doesNotContain("Potion");
-			assertThat(name).doesNotContain("Trinket");
-			assertThat(name).doesNotContain("Viktor");
-			assertThat(name).doesNotContain("Flask");
-			assertThat(name).doesNotContain("Enchantment");
-			assertThat(name).doesNotContain("Doran");
-			assertThat(name).doesNotContain("Quick");
-		});
-		assertThat(forTrollBuild).extracting(Item::getGroup).filteredOn(Objects::nonNull)
-				.allSatisfy((Consumer<String>) group -> {
-					assertThat(group).doesNotContain("FlaskGroup");
-					assertThat(group).doesNotContain("RelicBase");
-		});
+		for (Integer map : MAPS) {
+			List<Item> forTrollBuild = itemsRepository.forTrollBuild(map);
+			assertThat(forTrollBuild).as("Items for Troll Build should not be empty for map: " + GameMapUtil.getNameFromId(map) + "(" + map + ")")
+					.isNotEmpty();
+			assertThat(forTrollBuild).doesNotHaveDuplicates();
+			assertThat(forTrollBuild).extracting(Item::getMaps)
+					.extracting((Extractor<SortedMap<Integer, Boolean>, Object>) input -> input.get(map))
+					.contains(true);
+			assertThat(forTrollBuild).extracting(Item::getGold)
+					.extracting("purchasable", Boolean.class)
+					.containsOnly(true);
+			assertThat(forTrollBuild).extracting(Item::getConsumed)
+					.containsNull();
+			assertThat(forTrollBuild).filteredOn(item -> item.getInto() != null).flatExtracting(Item::getInto)
+					.isEmpty();
+			assertThat(forTrollBuild).doesNotContain(itemsResponse.getItems().get("1001"));
+			assertThat(forTrollBuild).extracting(Item::getDescription).allSatisfy((Consumer<String>) description -> {
+				assertThat(description).isNotNull();
+				assertThat(description).doesNotContain("Movement");
+				assertThat(description).doesNotContain("Potion");
+				assertThat(description).doesNotContain("Trinket");
+			});
+			assertThat(forTrollBuild).extracting(Item::getRequiredChampion).allSatisfy((Consumer<String>) requiredChampion -> {
+				assertThat(requiredChampion).isNull();
+			});
+			assertThat(forTrollBuild).extracting(Item::getRequiredAlly).allSatisfy((Consumer<String>) requiredAlly -> {
+				assertThat(requiredAlly).isNull();
+			});
+			assertThat(forTrollBuild).extracting(Item::getName).allSatisfy((Consumer<String>) name -> {
+				assertThat(name).isNotNull();
+				assertThat(name).doesNotContain("Movement");
+				assertThat(name).doesNotContain("Potion");
+				assertThat(name).doesNotContain("Trinket");
+				assertThat(name).doesNotContain("Viktor");
+				assertThat(name).doesNotContain("Flask");
+				assertThat(name).doesNotContain("Enchantment");
+				assertThat(name).doesNotContain("Doran");
+				assertThat(name).doesNotContain("Quick");
+			});
+			assertThat(forTrollBuild).extracting(Item::getGroup).filteredOn(Objects::nonNull)
+					.allSatisfy((Consumer<String>) group -> {
+						assertThat(group).doesNotContain("FlaskGroup");
+						assertThat(group).doesNotContain("RelicBase");
+					});
+		}
 	}
 
 }
