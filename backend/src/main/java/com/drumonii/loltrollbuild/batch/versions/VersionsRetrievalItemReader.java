dbe1b689ee1210ed42ec21505042b805e5f1667f
drumonii <drumonii@users.noreply.github.com>
2018-09-02 12:58:22 -0400

    Versions retrieval item reader to extend ListItemReader

--- backend/src/main/java/com/drumonii/loltrollbuild/batch/versions/VersionsRetrievalItemReader.java
+++ backend/src/main/java/com/drumonii/loltrollbuild/batch/versions/VersionsRetrievalItemReader.java
@@ -2,26 +2,17 @@ package com.drumonii.loltrollbuild.batch.versions;
 
 import com.drumonii.loltrollbuild.model.Version;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.support.ListItemReader;
 
 import java.util.List;
 
 /**
  * {@link ItemReader} for reading {@link Version}s from Riot's API.
  */
-public class VersionsRetrievalItemReader implements ItemReader<Version> {
+public class VersionsRetrievalItemReader extends ListItemReader<Version> {
 
-	private List<Version> versions;
-
-	public VersionsRetrievalItemReader(List<Version> versions) {
-		this.versions = versions;
-	}
-
-	@Override
-	public Version read() {
-		if (!versions.isEmpty()) {
-			return versions.remove(0);
-		}
-		return null;
+	public VersionsRetrievalItemReader(List<Version> list) {
+		super(list);
 	}
 
 }
