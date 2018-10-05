39d3cbeacab9eeb67a1995c383898873b3df2aed
drumonii <drumonii@users.noreply.github.com>
2018-10-04 22:09:53 -0400

    logger service to delegate to console depending on if in dev mode

--- frontend/src/app/shared/service/logger.service.spec.ts
+++ frontend/src/app/shared/service/logger.service.spec.ts
@@ -3,10 +3,68 @@ import { TestBed } from '@angular/core/testing';
 import { Logger } from './logger.service';
 
 describe('Logger', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let logger: Logger;
+  let debugSpy: jasmine.Spy;
+  let logSpy: jasmine.Spy;
+  let warnSpy: jasmine.Spy;
+  let errorSpy: jasmine.Spy;
 
-  it('should be created', () => {
-    const service: Logger = TestBed.get(Logger);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    this.logger = TestBed.get(Logger);
+
+    debugSpy = spyOn(console, 'debug');
+    logSpy = spyOn(console, 'log');
+    warnSpy = spyOn(console, 'warn');
+    errorSpy = spyOn(console, 'error');
   });
+
+  describe('debug', () => {
+
+    it('should delegate to console.debug', () => {
+      this.logger.debug('message');
+      expect(debugSpy).toHaveBeenCalledWith('message');
+
+      this.logger.debug('message', 'param1', 'param2');
+      expect(debugSpy).toHaveBeenCalledWith('message', 'param1', 'param2');
+    });
+
+  });
+
+  describe('info', () => {
+
+    it('should delegate to console.log', () => {
+      this.logger.info('message');
+      expect(logSpy).toHaveBeenCalledWith('message');
+
+      this.logger.info('message', 'param1', 'param2');
+      expect(logSpy).toHaveBeenCalledWith('message', 'param1', 'param2');
+    });
+
+  });
+
+  describe('warn', () => {
+
+    it('should delegate to console.warn', () => {
+      this.logger.warn('message');
+      expect(warnSpy).toHaveBeenCalledWith('message');
+
+      this.logger.warn('message', 'param1', 'param2');
+      expect(warnSpy).toHaveBeenCalledWith('message', 'param1', 'param2');
+    });
+
+  });
+
+  describe('error', () => {
+
+    it('should delegate to console.error', () => {
+      this.logger.error('message');
+      expect(errorSpy).toHaveBeenCalledWith('message');
+
+      this.logger.error('message', 'param1', 'param2');
+      expect(errorSpy).toHaveBeenCalledWith('message', 'param1', 'param2');
+    });
+
+  });
+
 });
--- frontend/src/app/shared/service/logger.service.ts
+++ frontend/src/app/shared/service/logger.service.ts
@@ -1,9 +1,68 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 
+/**
+ * Logging service that delegates to the console depending on log level: info, warn, error and whether if in
+ * development mode.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class Logger {
 
-  constructor() { }
+  private readonly isDev: boolean;
+
+  constructor() {
+    this.isDev = isDevMode();
+  }
+
+  /**
+   * Logs the message and params, if present, as a console.debug only if in development mode.
+   *
+   * @param message the debug message
+   * @param optionalParams the optional params
+   */
+  debug(message: any, ...optionalParams: any[]): void {
+    if (this.isDev) {
+      // tslint:disable-next-line
+      console.debug(message, ...optionalParams);
+    }
+  }
+
+  /**
+   * Logs the message and params, if present, as a console.log only if in development mode.
+   *
+   * @param message the log message
+   * @param optionalParams the optional params
+   */
+  info(message: any, ...optionalParams: any[]): void {
+    if (this.isDev) {
+      // tslint:disable-next-line
+      console.log(message, ...optionalParams);
+    }
+  }
+
+  /**
+   * Logs the message and params, if present, as a console.warn only if in development mode.
+   *
+   * @param message the warn message
+   * @param optionalParams the optional params
+   */
+  warn(message: any, ...optionalParams: any[]): void {
+    if (this.isDev) {
+      // tslint:disable-next-line
+      console.warn(message, ...optionalParams);
+    }
+  }
+
+  /**
+   * Logs the message and params, if present, as a console.error regardless of if in development mode.
+   *
+   * @param message the error message
+   * @param optionalParams the optional params
+   */
+  error(message: any, ...optionalParams: any[]): void {
+    // tslint:disable-next-line
+    console.error(message, ...optionalParams);
+  }
+
 }
