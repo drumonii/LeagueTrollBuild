ef87c02c07c3378db78fbbbb0e7d65a1f7c9b3a5
drumonii <drumonii@users.noreply.github.com>
2018-11-03 23:27:05 -0400

    Implement admin already logged in guard for /admin/login route

--- frontend/src/app/admin/guard/admin-already-logged-in.guard.spec.ts
+++ frontend/src/app/admin/guard/admin-already-logged-in.guard.spec.ts
@@ -1,15 +1,43 @@
 import { inject, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
+import { of } from 'rxjs';
+
+import { AdminAuthService } from '@security/admin-auth.service';
 import { AdminAlreadyLoggedInGuard } from './admin-already-logged-in.guard';
 
 describe('AdminAlreadyLoggedInGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
       providers: [AdminAlreadyLoggedInGuard]
     });
   });
 
-  it('should ...', inject([AdminAlreadyLoggedInGuard], (guard: AdminAlreadyLoggedInGuard) => {
-    expect(guard).toBeTruthy();
+  beforeEach(inject([Router], (router: Router) => {
+    spyOn(router, 'navigate');
   }));
+
+  it('should return false and redirect to /admin if admin is already logged in',
+    inject([AdminAlreadyLoggedInGuard, AdminAuthService, Router], (guard: AdminAlreadyLoggedInGuard, authService: AdminAuthService, router: Router) => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(of(true));
+
+    guard.canActivate(null, null).subscribe(canActivate => {
+      expect(canActivate).toBe(false);
+      expect(router.navigate).toHaveBeenCalledWith(['/admin']);
+    });
+  }));
+
+  it('should return true if admin is not authenticated',
+    inject([AdminAlreadyLoggedInGuard, AdminAuthService, Router], (guard: AdminAlreadyLoggedInGuard, authService: AdminAuthService, router: Router) => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(of(false));
+
+    guard.canActivate(null, null).subscribe(canActivate => {
+      expect(canActivate).toBe(true);
+      expect(router.navigate).not.toHaveBeenCalled();
+    });
+  }));
+
 });
--- frontend/src/app/admin/guard/admin-already-logged-in.guard.ts
+++ frontend/src/app/admin/guard/admin-already-logged-in.guard.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 
 import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+import { AdminAuthService } from '@security/admin-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAlreadyLoggedInGuard implements CanActivate {
 
+  constructor(private authService: AdminAuthService, private router: Router) {}
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return undefined;
+    return this.authService.isAuthenticated()
+      .pipe(
+        take(1),
+        map((isAdminLoggedIn) => {
+          if (isAdminLoggedIn) {
+            this.redirectToAdminHome();
+            return false;
+          }
+          return true;
+        })
+      );
+  }
+
+  private redirectToAdminHome() {
+    this.router.navigate(['/admin']);
   }
 
 }
