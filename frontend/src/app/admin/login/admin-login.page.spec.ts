ee1cd32c469538a93f06eed4be98eb87ecd2ff8d
drumonii <drumonii@users.noreply.github.com>
2018-11-04 00:14:10 -0400

    Attempt to login admin on valid form submission
    Redirect to admin home on successful reactive form login.

--- frontend/src/app/admin/login/admin-login.page.spec.ts
+++ frontend/src/app/admin/login/admin-login.page.spec.ts
@@ -4,11 +4,13 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 import { By } from '@angular/platform-browser';
 
-import { AdminAuthService } from '@security/admin-auth.service';
+import { of } from 'rxjs';
 
+import { AdminAuthService } from '@security/admin-auth.service';
+import { TitleService } from '@service/title.service';
 import { AdminLoginModule } from './admin-login.module';
 import { AdminLoginPage } from './admin-login.page';
-import { TitleService } from '@service/title.service';
+import { AdminLoginResponse, AdminLoginStatus } from '@security/admin-login-response';
 
 describe('AdminLoginPage', () => {
   let component: AdminLoginPage;
@@ -83,12 +85,36 @@ describe('AdminLoginPage', () => {
 
     describe('with bad credentials', () => {
 
-      xit('should show bad credentials alert', inject([AdminAuthService, Router], (authService: AdminAuthService, router: Router) => {
+      it('should show bad credentials alert', inject([AdminAuthService, Router], (authService: AdminAuthService, router: Router) => {
+        const failedLoginResponse: AdminLoginResponse = {
+          status: AdminLoginStatus.FAILED,
+          message: 'Bad credentials',
+        };
+
+        spyOn(authService, 'loginAdmin').and.returnValue(of(failedLoginResponse));
         spyOn(router, 'navigate');
+
+        component.unexpectedError = true;
+        component.loggedOut = true;
+
+        const username = component.adminLoginForm.get('username');
+        username.setValue('some_username'); username.markAsTouched();
+
+        const password = component.adminLoginForm.get('password');
+        password.setValue('some_password'); password.markAsTouched();
+
+        fixture.detectChanges();
+
+        const loginBtn = fixture.debugElement.query(By.css('#login-btn'));
+        loginBtn.nativeElement.click();
 
         fixture.detectChanges();
 
         expect(fixture.debugElement.query(By.css('#login-bad-credentials-alert'))).toBeTruthy();
+        expect(fixture.debugElement.query(By.css('#login-unexpected-error-alert'))).toBeFalsy();
+        expect(fixture.debugElement.query(By.css('#logged-out-alert'))).toBeFalsy();
+
+        expect(authService.loginAdmin).toHaveBeenCalledWith(username.value, password.value);
         expect(router.navigate).not.toHaveBeenCalled();
       }));
 
@@ -96,12 +122,31 @@ describe('AdminLoginPage', () => {
 
     describe('with unexpected login error', () => {
 
-      xit('should show unexpected login error alert', inject([AdminAuthService, Router], (authService: AdminAuthService, router: Router) => {
+      it('should show unexpected login error alert', inject([AdminAuthService, Router], (authService: AdminAuthService, router: Router) => {
+        spyOn(authService, 'loginAdmin').and.returnValue(of(null));
         spyOn(router, 'navigate');
+
+        component.badCredentials = true;
+        component.loggedOut = true;
+
+        const username = component.adminLoginForm.get('username');
+        username.setValue('some_username'); username.markAsTouched();
+
+        const password = component.adminLoginForm.get('password');
+        password.setValue('some_password'); password.markAsTouched();
 
         fixture.detectChanges();
 
+        const loginBtn = fixture.debugElement.query(By.css('#login-btn'));
+        loginBtn.nativeElement.click();
+
+        fixture.detectChanges();
+
+        expect(fixture.debugElement.query(By.css('#login-bad-credentials-alert'))).toBeFalsy();
         expect(fixture.debugElement.query(By.css('#login-unexpected-error-alert'))).toBeTruthy();
+        expect(fixture.debugElement.query(By.css('#logged-out-alert'))).toBeFalsy();
+
+        expect(authService.loginAdmin).toHaveBeenCalledWith(username.value, password.value);
         expect(router.navigate).not.toHaveBeenCalled();
       }));
 
@@ -109,14 +154,37 @@ describe('AdminLoginPage', () => {
 
     describe('with correct credentials', () => {
 
-      xit('should redirect to admin home', inject([AdminAuthService, Router], (authService: AdminAuthService, router: Router) => {
+      it('should redirect to admin home', inject([AdminAuthService, Router], (authService: AdminAuthService, router: Router) => {
+        const successfulLoginResponse: AdminLoginResponse = {
+          status: AdminLoginStatus.SUCCESS,
+          message: 'Login successful',
+          userDetails: {
+            username: 'admin',
+            authorities: [
+              {
+                authority: 'ROLE_ADMIN'
+              }
+            ]
+          }
+        };
+
+        spyOn(authService, 'loginAdmin').and.returnValue(of(successfulLoginResponse));
         spyOn(router, 'navigate');
+
+        const username = component.adminLoginForm.get('username');
+        username.setValue('some_username'); username.markAsTouched();
+
+        const password = component.adminLoginForm.get('password');
+        password.setValue('some_password'); password.markAsTouched();
+
+        fixture.detectChanges();
 
         const loginBtn = fixture.debugElement.query(By.css('#login-btn'));
         loginBtn.nativeElement.click();
 
         fixture.detectChanges();
 
+        expect(authService.loginAdmin).toHaveBeenCalledWith(username.value, password.value);
         expect(router.navigate).toHaveBeenCalledWith(['/admin']);
       }));
 
--- frontend/src/app/admin/login/admin-login.page.ts
+++ frontend/src/app/admin/login/admin-login.page.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { finalize } from 'rxjs/operators';
+
 import { TitleService } from '@service/title.service';
 import { AdminAuthService } from '@security/admin-auth.service';
+import { AdminLoginStatus } from '@security/admin-login-response';
 
 @Component({
   selector: 'ltb-admin-login',
@@ -35,9 +38,30 @@ export class AdminLoginPage implements OnInit {
 
   onSubmit(): void {
     if (this.adminLoginForm.valid) {
-      // TODO: Check authentication with auth service
-      this.router.navigate(['/admin']);
+      this.isLoggingIn = true;
+      this.resetAlerts();
+      this.authService.loginAdmin(this.adminLoginForm.get('username').value, this.adminLoginForm.get('password').value)
+        .pipe(
+          finalize(() => this.isLoggingIn = false)
+        )
+        .subscribe((loginResponse) => {
+          if (loginResponse) {
+            if (loginResponse.status === AdminLoginStatus.SUCCESS) {
+              this.router.navigate(['/admin']);
+            } else if (loginResponse.status === AdminLoginStatus.FAILED) {
+              this.badCredentials = true;
+            }
+          } else {
+            this.unexpectedError = true;
+          }
+        });
     }
+  }
+
+  private resetAlerts(): void {
+    this.badCredentials = false;
+    this.unexpectedError = false;
+    this.loggedOut = false;
   }
 
 }
