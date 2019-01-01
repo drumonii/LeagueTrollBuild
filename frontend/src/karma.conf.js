7f1505c0184d0a866d5ee894611607af07f412fb
drumonii <drumonii@users.noreply.github.com>
2019-01-01 13:44:48 -0500

    karma ie launcher

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
+      require('karma-ie-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -29,7 +30,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox', 'IE'],
     singleRun: false
   });
 };
