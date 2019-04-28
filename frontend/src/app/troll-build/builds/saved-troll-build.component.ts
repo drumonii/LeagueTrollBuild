fb10ee455a34c40c71155746eba01a7983077c6f
drumonii <drumonii@users.noreply.github.com>
2019-04-28 19:21:04 -0400

    Saved troll build component change detection should be OnPush

--- frontend/src/app/troll-build/builds/saved-troll-build.component.ts
+++ frontend/src/app/troll-build/builds/saved-troll-build.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Build } from '@ltb-model/build';
 
 @Component({
   selector: 'ltb-saved-troll-build',
   templateUrl: './saved-troll-build.component.html',
-  styleUrls: ['./saved-troll-build.component.scss']
+  styleUrls: ['./saved-troll-build.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SavedTrollBuildComponent implements OnInit {
 
