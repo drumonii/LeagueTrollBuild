21234518afecb67fb82f6dae2b1b7acc10877cca
drumonii <drumonii@users.noreply.github.com>
2020-12-06 17:17:42 -0500

    Prevent caching index.html
    This allows for the latest built js and css to always be fetched while still allowing them to be cached.

--- backend/src/main/java/com/drumonii/loltrollbuild/config/WebMvcConfig.java
+++ backend/src/main/java/com/drumonii/loltrollbuild/config/WebMvcConfig.java
@@ -3,9 +3,13 @@ package com.drumonii.loltrollbuild.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration(proxyBeanMethods = false)
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -17,6 +21,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix(apiPath, clazz -> clazz.isAnnotationPresent(RestController.class) &&
                 !AbstractErrorController.class.isAssignableFrom(clazz));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**/index.html")
+                .addResourceLocations("classpath:/public/")
+                .setCacheControl(CacheControl.noStore());
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/")
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
     }
 
 }
---
+++ backend/src/test/java/com/drumonii/loltrollbuild/config/WebMvcConfigTest.java
@@ -0,0 +1,163 @@
+package com.drumonii.loltrollbuild.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Map;
+
+import static com.drumonii.loltrollbuild.config.Profiles.DDRAGON;
+import static com.drumonii.loltrollbuild.config.Profiles.TESTING;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles({ TESTING, DDRAGON })
+class WebMvcConfigTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Nested
+    @DisplayName("admin static resources")
+    class admin {
+
+        @Test
+        void indexHtmlHasNoStoreCacheControl() throws Exception {
+            SimpleUrlHandlerMapping handlerMapping = getSimpleUrlHandlerMapping();
+            Map<String, Object> handlerMap = handlerMapping.getHandlerMap();
+            assertThat(handlerMap).hasSize(2);
+
+            ResourceHttpRequestHandler resourceHandler = (ResourceHttpRequestHandler) handlerMap.get("/**/index.html");
+
+            MockHttpServletRequest request = new MockHttpServletRequest();
+            request.setMethod(HttpMethod.GET.name());
+            request.setRequestURI("/admin/index.html");
+            request.setAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE, "admin/index.html");
+
+            MockHttpServletResponse response = new MockHttpServletResponse();
+
+            HandlerExecutionChain chain = handlerMapping.getHandler(request);
+            assertThat(chain.getHandler()).isEqualTo(resourceHandler);
+
+            resourceHandler.handleRequest(request, response);
+
+            assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE);
+            assertThat(response.getHeaderNames()).contains("Cache-Control");
+            assertThat(response.getHeader("Cache-Control")).isEqualTo("no-store");
+        }
+
+        @CsvSource({
+                "main.85b446f791ee3b18e1c5.js, application/javascript",
+                "styles.26c46902ddc723992338.css, text/css"
+        })
+        @ParameterizedTest(name = "resource=''{0}'', contentType=''{1}''")
+        void staticResourceHasMaxAgeCacheControl(String resource, String contentType) throws Exception {
+            SimpleUrlHandlerMapping handlerMapping = getSimpleUrlHandlerMapping();
+            Map<String, Object> handlerMap = handlerMapping.getHandlerMap();
+            assertThat(handlerMap).hasSize(2);
+
+            ResourceHttpRequestHandler resourceHandler = (ResourceHttpRequestHandler) handlerMap.get("/**");
+
+            MockHttpServletRequest request = new MockHttpServletRequest();
+            request.setMethod(HttpMethod.GET.name());
+            request.setRequestURI("/admin/" + resource);
+            request.setAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE, "admin/" + resource);
+
+            MockHttpServletResponse response = new MockHttpServletResponse();
+
+            HandlerExecutionChain chain = handlerMapping.getHandler(request);
+            assertThat(chain.getHandler()).isEqualTo(resourceHandler);
+
+            resourceHandler.handleRequest(request, response);
+
+            assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.getContentType()).isEqualTo(contentType);
+            assertThat(response.getHeaderNames()).contains("Cache-Control");
+            assertThat(response.getHeader("Cache-Control")).isEqualTo("max-age=31536000");
+        }
+
+    }
+
+    @Nested
+    @DisplayName("troll build static resources")
+    class trollBuild {
+
+        @Test
+        void indexHtmlHasNoStoreCacheControl() throws Exception {
+            SimpleUrlHandlerMapping handlerMapping = getSimpleUrlHandlerMapping();
+            Map<String, Object> handlerMap = handlerMapping.getHandlerMap();
+            assertThat(handlerMap).hasSize(2);
+
+            ResourceHttpRequestHandler resourceHandler = (ResourceHttpRequestHandler) handlerMap.get("/**/index.html");
+
+            MockHttpServletRequest request = new MockHttpServletRequest();
+            request.setMethod(HttpMethod.GET.name());
+            request.setRequestURI("/troll-build/index.html");
+            request.setAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE, "troll-build/index.html");
+
+            MockHttpServletResponse response = new MockHttpServletResponse();
+
+            HandlerExecutionChain chain = handlerMapping.getHandler(request);
+            assertThat(chain.getHandler()).isEqualTo(resourceHandler);
+
+            resourceHandler.handleRequest(request, response);
+
+            assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.getContentType()).isEqualTo(MediaType.TEXT_HTML_VALUE);
+            assertThat(response.getHeaderNames()).contains("Cache-Control");
+            assertThat(response.getHeader("Cache-Control")).isEqualTo("no-store");
+        }
+
+        @CsvSource({
+                "main.56796bffe1238c65623f.js, application/javascript",
+                "styles.64f6947700cdf03522dd.css, text/css"
+        })
+        @ParameterizedTest(name = "resource=''{0}'', contentType=''{1}''")
+        void staticResourceHasMaxAgeCacheControl(String resource, String contentType) throws Exception {
+            SimpleUrlHandlerMapping handlerMapping = getSimpleUrlHandlerMapping();
+            Map<String, Object> handlerMap = handlerMapping.getHandlerMap();
+            assertThat(handlerMap).hasSize(2);
+
+            ResourceHttpRequestHandler resourceHandler = (ResourceHttpRequestHandler) handlerMap.get("/**");
+
+            MockHttpServletRequest request = new MockHttpServletRequest();
+            request.setMethod(HttpMethod.GET.name());
+            request.setRequestURI("/troll-build/" + resource);
+            request.setAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE, "troll-build/" + resource);
+
+            MockHttpServletResponse response = new MockHttpServletResponse();
+
+            HandlerExecutionChain chain = handlerMapping.getHandler(request);
+            assertThat(chain.getHandler()).isEqualTo(resourceHandler);
+
+            resourceHandler.handleRequest(request, response);
+
+            assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.getContentType()).isEqualTo(contentType);
+            assertThat(response.getHeaderNames()).contains("Cache-Control");
+            assertThat(response.getHeader("Cache-Control")).isEqualTo("max-age=31536000");
+        }
+
+    }
+
+    private SimpleUrlHandlerMapping getSimpleUrlHandlerMapping() {
+        return applicationContext.getBean(SimpleUrlHandlerMapping.class);
+    }
+
+}
