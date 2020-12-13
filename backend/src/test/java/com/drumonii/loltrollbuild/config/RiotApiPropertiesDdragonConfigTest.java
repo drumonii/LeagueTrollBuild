2d7ff0216de1f327a14da4f2def3dc7b10aa9e15
drumonii <drumonii@users.noreply.github.com>
2020-12-13 18:35:13 -0500

    Use nested tests in RiotApiPropertiesDdragonConfigTest

--- backend/src/test/java/com/drumonii/loltrollbuild/config/RiotApiPropertiesDdragonConfigTest.java
+++ backend/src/test/java/com/drumonii/loltrollbuild/config/RiotApiPropertiesDdragonConfigTest.java
@@ -3,6 +3,8 @@ package com.drumonii.loltrollbuild.config;
 import com.drumonii.loltrollbuild.riot.api.RiotApiProperties;
 import com.drumonii.loltrollbuild.riot.api.RiotApiProperties.Ddragon;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +30,7 @@ import static com.drumonii.loltrollbuild.config.Profiles.TESTING;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-@ExtendWith({SpringExtension.class})
+@ExtendWith({ SpringExtension.class })
 @Import(RiotApiConfig.class)
 @ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
 @EnableConfigurationProperties(RiotApiProperties.class)
@@ -38,9 +40,6 @@ class RiotApiPropertiesDdragonConfigTest {
 
 	@Autowired
 	private RiotApiProperties riotProperties;
-
-	@Autowired
-	private ApplicationContext applicationContext;
 
 	private Ddragon ddragon;
 
@@ -54,180 +53,144 @@ class RiotApiPropertiesDdragonConfigTest {
 		locale = ddragon.getLocale();
 	}
 
-	/*
-	 * RestTemplate Tests
-	 */
+	@Nested
+	@DisplayName("RestTemplate Tests")
+	class RestTemplateTests {
 
-	@Test
-	void restTemplate() {
-		assertThatCode(() -> {
-			RestTemplate restTemplate = applicationContext.getBean("restTemplate", RestTemplate.class);
-			assertThat(restTemplate.getInterceptors()).hasSize(1);
-			assertThat(restTemplate.getMessageConverters()).flatExtracting(HttpMessageConverter::getSupportedMediaTypes)
-					.contains(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM,
-							MediaType.parseMediaType("binary/octet-stream"), MediaType.parseMediaType("text/json;charset=UTF-8"));
-		}).doesNotThrowAnyException();
+		@Test
+		void restTemplate(ApplicationContext applicationContext) {
+			assertThatCode(() -> {
+				RestTemplate restTemplate = applicationContext.getBean("restTemplate", RestTemplate.class);
+				assertThat(restTemplate.getInterceptors()).hasSize(1);
+				assertThat(restTemplate.getMessageConverters()).flatExtracting(HttpMessageConverter::getSupportedMediaTypes)
+						.contains(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM,
+								MediaType.parseMediaType("binary/octet-stream"), MediaType.parseMediaType("text/json;charset=UTF-8"));
+			}).doesNotThrowAnyException();
+		}
+
 	}
 
-	/*
-	 * Summoner Spells Uri Components Tests
-	 */
+	@Nested
+	@DisplayName("Summoner Spells Uri Components Tests")
+	class SummonerSpellsUriComponentsTests {
 
-	@Autowired
-	@Qualifier("summonerSpells")
-	private UriComponentsBuilder summonerSpellsUriBuilder;
+		@Test
+		void summonerSpellsUri(@Qualifier("summonerSpells") UriComponentsBuilder summonerSpellsUriBuilder) {
+			UriComponents summonerSpellsUri = summonerSpellsUriBuilder.buildAndExpand(patch, locale);
+			assertThat(summonerSpellsUri.toString()).as("Summoner Spells URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getSummonerSpells().replace("{version}", patch).replace("{locale}", locale));
+		}
 
-	@Test
-	void summonerSpellsUri() {
-		UriComponents summonerSpellsUri = summonerSpellsUriBuilder.buildAndExpand(patch, locale);
-		assertThat(summonerSpellsUri.toString()).as("Summoner Spells URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getSummonerSpells().replace("{version}", patch).replace("{locale}", locale));
+		@Test
+		void summonerSpellImgUri(@Qualifier("summonerSpellsImg") UriComponentsBuilder summonerSpellImgBuilder) {
+			String imgFull = "SummonerBoost.png";
+			UriComponents summonerSpellImgUri = summonerSpellImgBuilder.buildAndExpand(patch, imgFull);
+			assertThat(summonerSpellImgUri.toString()).as("Summoner Spells Image URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getSummonerSpellsImg().replace("{version}", patch)
+							.replace("{summonerSpellImgFull}", imgFull));
+		}
+
 	}
 
-	@Autowired
-	@Qualifier("summonerSpellsImg")
-	private UriComponentsBuilder summonerSpellImgBuilder;
+	@Nested
+	@DisplayName("Items Uri Components Tests")
+	class ItemsUriComponentsTests {
 
-	@Test
-	void summonerSpellImgUri() {
-		String imgFull = "SummonerBoost.png";
-		UriComponents summonerSpellImgUri = summonerSpellImgBuilder.buildAndExpand(patch, imgFull);
-		assertThat(summonerSpellImgUri.toString()).as("Summoner Spells Image URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getSummonerSpellsImg().replace("{version}", patch)
-						.replace("{summonerSpellImgFull}", imgFull));
+		@Test
+		void itemsUri(@Qualifier("items") UriComponentsBuilder itemsUriBuilder) {
+			UriComponents itemsUri = itemsUriBuilder.buildAndExpand(patch, locale);
+			assertThat(itemsUri.toString()).as("Items URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getItems().replace("{version}", patch).replace("{locale}", locale));
+		}
+
+		@Test
+		void itemsImgUri(@Qualifier("itemsImg") UriComponentsBuilder itemsImgBuilder) {
+			String imgFull = "1.png";
+			UriComponents itemsImgUri = itemsImgBuilder.buildAndExpand(patch, imgFull);
+			assertThat(itemsImgUri.toString()).as("Items Image URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getItemsImg().replace("{version}", patch).replace("{itemImgFull}", imgFull));
+		}
+
 	}
 
-	/*
-	 * Items Uri Components Tests
-	 */
+	@Nested
+	@DisplayName("Champions Uri Components Tests")
+	class ChampionsUriComponentsTests {
 
-	@Autowired
-	@Qualifier("items")
-	private UriComponentsBuilder itemsUriBuilder;
+		@Test
+		void championsUri(@Qualifier("champions") UriComponentsBuilder championsUriBuilder) {
+			UriComponents championsUri = championsUriBuilder.buildAndExpand(patch, locale);
+			assertThat(championsUri.toString()).as("Champions URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampions().replace("{version}", patch).replace("{locale}", locale));
+		}
 
-	@Test
-	void itemsUri() {
-		UriComponents itemsUri = itemsUriBuilder.buildAndExpand(patch, locale);
-		assertThat(itemsUri.toString()).as("Items URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getItems().replace("{version}", patch).replace("{locale}", locale));
+		@Test
+		void championUri(@Qualifier("champion") UriComponentsBuilder championUriBuilder) {
+			int id = 1;
+			UriComponents championUri = championUriBuilder.buildAndExpand(patch, locale, id);
+			assertThat(championUri.toString()).as("Champion URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampion().replace("{version}", patch)
+							.replace("{locale}", locale).replace("{id}", String.valueOf(id)));
+		}
+
+		@Test
+		void championsImgUri(@Qualifier("championsImg") UriComponentsBuilder championsImgBuilder) {
+			String imgFull = "Champion.png";
+			UriComponents championsImgUri = championsImgBuilder.buildAndExpand(patch, imgFull);
+			assertThat(championsImgUri.toString()).as("Champions Image URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampionsImg().replace("{version}", patch).replace("{championImgFull}", imgFull));
+		}
+
+		@Test
+		void championsSpellImgUri(@Qualifier("championsSpellImg") UriComponentsBuilder championsSpellImgBuilder) {
+			String spellImgFull = "ChampionSpell.png";
+			UriComponents championsSpellImgUri = championsSpellImgBuilder.buildAndExpand(patch, spellImgFull);
+			assertThat(championsSpellImgUri.toString()).as("Champion Spell Image URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampionsSpellImg().replace("{version}", patch)
+							.replace("{championSpellImgFull}", spellImgFull));
+		}
+
+		@Test
+		void championsPassiveImgUri(@Qualifier("championsPassiveImg") UriComponentsBuilder championsPassiveImgBuilder) {
+			String spellImgFull = "ChampionPassive.png";
+			UriComponents championsPassiveImgUri = championsPassiveImgBuilder.buildAndExpand(patch, spellImgFull);
+			assertThat(championsPassiveImgUri.toString()).as("Champion Passive Image URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampionsPassiveImg().replace("{version}", patch)
+							.replace("{championPassiveImgFull}", spellImgFull));
+		}
+
 	}
 
-	@Autowired
-	@Qualifier("itemsImg")
-	private UriComponentsBuilder itemsImgBuilder;
+	@Nested
+	@DisplayName("Maps Uri Components Tests")
+	class MapsUriComponentsTests {
 
-	@Test
-	void itemsImgUri() {
-		String imgFull = "1.png";
-		UriComponents itemsImgUri = itemsImgBuilder.buildAndExpand(patch, imgFull);
-		assertThat(itemsImgUri.toString()).as("Items Image URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getItemsImg().replace("{version}", patch).replace("{itemImgFull}", imgFull));
+		@Test
+		void mapsUri(@Qualifier("maps") UriComponentsBuilder mapsUriBuilder) {
+			UriComponents mapsUri = mapsUriBuilder.buildAndExpand(patch, locale);
+			assertThat(mapsUri.toString()).as("Maps URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getMaps().replace("{version}", patch).replace("{locale}", locale));
+		}
+
+		@Test
+		void mapsImgUri(@Qualifier("mapsImg") UriComponentsBuilder mapsImgBuilder) {
+			String imgFull = "Map.png";
+			UriComponents mapsImgUri = mapsImgBuilder.buildAndExpand(patch, imgFull);
+			assertThat(mapsImgUri.toString()).as("Maps Image URI")
+					.isEqualTo(ddragon.getBaseUrl() + ddragon.getMapsImg().replace("{version}", patch).replace("{mapImgFull}", imgFull));
+		}
+
 	}
 
-	/*
-	 * Champions Uri Components Tests
-	 */
+	@Nested
+	@DisplayName("Versions Uri Components Tests")
+	class VersionsUriComponentsTests {
 
-	@Autowired
-	@Qualifier("champions")
-	private UriComponentsBuilder championsUriBuilder;
+		@Test
+		void versionsUri(@Qualifier("versions") UriComponents versionsUri) {
+			assertThat(versionsUri.toString()).as("Versions URI").isEqualTo(ddragon.getBaseUrl() + ddragon.getVersions());
+		}
 
-	@Test
-	void championsUri() {
-		UriComponents championsUri = championsUriBuilder.buildAndExpand(patch, locale);
-		assertThat(championsUri.toString()).as("Champions URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampions().replace("{version}", patch).replace("{locale}", locale));
-	}
-
-	@Autowired
-	@Qualifier("champion")
-	private UriComponentsBuilder championUriBuilder;
-
-	@Test
-	void championUri() {
-		int id = 1;
-		UriComponents championUri = championUriBuilder.buildAndExpand(patch, locale, id);
-		assertThat(championUri.toString()).as("Champion URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampion().replace("{version}", patch)
-						.replace("{locale}", locale).replace("{id}", String.valueOf(id)));
-	}
-
-	@Autowired
-	@Qualifier("championsImg")
-	private UriComponentsBuilder championsImgBuilder;
-
-	@Test
-	void championsImgUri() {
-		String imgFull = "Champion.png";
-		UriComponents championsImgUri = championsImgBuilder.buildAndExpand(patch, imgFull);
-		assertThat(championsImgUri.toString()).as("Champions Image URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampionsImg().replace("{version}", patch).replace("{championImgFull}", imgFull));
-	}
-
-	@Autowired
-	@Qualifier("championsSpellImg")
-	private UriComponentsBuilder championsSpellImgBuilder;
-
-	@Test
-	void championsSpellImgUri() {
-		String spellImgFull = "ChampionSpell.png";
-		UriComponents championsSpellImgUri = championsSpellImgBuilder.buildAndExpand(patch, spellImgFull);
-		assertThat(championsSpellImgUri.toString()).as("Champion Spell Image URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampionsSpellImg().replace("{version}", patch)
-						.replace("{championSpellImgFull}", spellImgFull));
-	}
-
-	@Autowired
-	@Qualifier("championsPassiveImg")
-	private UriComponentsBuilder championsPassiveImgBuilder;
-
-	@Test
-	void championsPassiveImgUri() {
-		String spellImgFull = "ChampionPassive.png";
-		UriComponents championsPassiveImgUri = championsPassiveImgBuilder.buildAndExpand(patch, spellImgFull);
-		assertThat(championsPassiveImgUri.toString()).as("Champion Passive Image URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getChampionsPassiveImg().replace("{version}", patch)
-						.replace("{championPassiveImgFull}", spellImgFull));
-	}
-
-	/*
-	 * Maps Uri Components Tests
-	 */
-
-	@Autowired
-	@Qualifier("maps")
-	private UriComponentsBuilder mapsUriBuilder;
-
-	@Test
-	void mapsUri() {
-		UriComponents mapsUri = mapsUriBuilder.buildAndExpand(patch, locale);
-		assertThat(mapsUri.toString()).as("Maps URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getMaps().replace("{version}", patch).replace("{locale}", locale));
-	}
-
-	@Autowired
-	@Qualifier("mapsImg")
-	private UriComponentsBuilder mapsImgBuilder;
-
-	@Test
-	void mapsImgUri() {
-		String imgFull = "Map.png";
-		UriComponents mapsImgUri = mapsImgBuilder.buildAndExpand(patch, imgFull);
-		assertThat(mapsImgUri.toString()).as("Maps Image URI")
-				.isEqualTo(ddragon.getBaseUrl() + ddragon.getMapsImg().replace("{version}", patch).replace("{mapImgFull}", imgFull));
-	}
-
-	/*
-	 * Versions Uri Components Tests
-	 */
-
-	@Autowired
-	@Qualifier("versions")
-	private UriComponents versionsUri;
-
-	@Test
-	void versionsUri() {
-		assertThat(versionsUri.toString()).as("Versions URI").isEqualTo(ddragon.getBaseUrl() + ddragon.getVersions());
 	}
 
 }
