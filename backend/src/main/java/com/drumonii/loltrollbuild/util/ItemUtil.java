d82fa225cd0e6f598dea560873d4580d5896f919
drumonii <drumonii@users.noreply.github.com>
2020-12-13 14:02:16 -0500

    Remove unused requiresAllyOrnn item util method

--- backend/src/main/java/com/drumonii/loltrollbuild/util/ItemUtil.java
+++ backend/src/main/java/com/drumonii/loltrollbuild/util/ItemUtil.java
@@ -9,17 +9,4 @@ public class ItemUtil {
 
 	private ItemUtil() {}
 
-	/**
-	 * Determines if the Item requires Ornn as an ally.
-	 *
-	 * @param colloq the colloq to inspect
-	 * @return {@code true} if requires Ornn, otherwise {@code false}
-	 */
-	public static boolean requiresAllyOrnn(String colloq) {
-		if (colloq == null) {
-			return false;
-		}
-		return colloq.contains("Ornn");
-	}
-
 }
--- backend/src/test/java/com/drumonii/loltrollbuild/util/ItemUtilTest.java
+++ backend/src/test/java/com/drumonii/loltrollbuild/util/ItemUtilTest.java
@@ -1,21 +1,5 @@
 package com.drumonii.loltrollbuild.util;
 
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class ItemUtilTest {
-
-	@Test
-	void determinesIfRequiresAllyOrnn() {
-		assertThat(ItemUtil.requiresAllyOrnn(null)).isFalse();
-
-		String colloq = "ie;Ornn;forge;Masterwork";
-
-		assertThat(ItemUtil.requiresAllyOrnn(colloq)).isTrue();
-
-		colloq = "colloq;triforce;tons of damage";
-		assertThat(ItemUtil.requiresAllyOrnn(colloq)).isFalse();
-	}
 
 }
